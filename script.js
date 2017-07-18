99d49970606902ea4dea3318d5f3d899d9fe70ec
Cheung <samuelcheung@kpmg.com>
2017-07-18 14:39:42 -0400

    Slideshow modal replacement

--- script.js
+++ script.js
@@ -37,6 +37,18 @@ $(document).on("scrollstart",function(){
 	document.getElementById("menu").style.height="1.5em";
 });
 */
+/*
+
+'<div class="slides">
+  <div class="numbertext">' + i +' / ' + images.length + '</div>
+	<img src=' + images[i] + ' style="width:100%">
+</div>'
+<a class="prev" onclick="changeSlides(-1)">&#10094;</a>
+<a class="next" onclick="changeSlides(1)">&#10095;</a>
+<div class="caption-container">
+  <p id="caption"></p>
+</div>
+*/
 
 function loadImages(){
 	var allImages = "";
@@ -45,9 +57,12 @@ function loadImages(){
 			"kitten6.jpeg", "P1.jpg", "P1Colored.jpg", "RalfSchumacher.jpg", "RedBull.jpg", "RedBullRain2.jpg",
 			"RedBullRain3.jpg", "Rosberg.jpg", "Rosberg2.jpg", "Rosberg3.jpg"];
 	for(var i=0; i<images.length; i++){
-		allImages = '<div class="gallery"> <a target="_blank" href='+images[i]+'> <img src='+images[i]+' class="img" id="img" alt=""> </a> </div>';
+		allImages = '<div class="gallery"> <a onclick="openModal();currentSlide(1);"> <img src='+images[i]+' class="img" id="img" alt=""> </a> </div>';
+		slideShow = '<div class="slides"> <div class="numbertext">' + i +' / ' + images.length + '</div> <img src=' + images[i] + ' class="modal-image""> </div>'
 		$('#photos').append(allImages);
+		$('.modal-content').append(slideShow);
 	}
+	$('.modal-content').append('<a class="prev" onclick="changeSlides(-1)">&#10094;</a> <a class="next" onclick="changeSlides(1)">&#10095;</a>');
 	var $images = $(".gallery");
 	
 	var loaded=0;
@@ -59,4 +74,38 @@ function loadImages(){
 			});
 		});
 	});
+	showSlides(slideIndex);
+}
+
+function openModal() {
+  document.getElementById('modal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('modal').style.display = "none";
+}
+
+var slideIndex = 1;
+function changeSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function changeSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function currentSlide(n){
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
